83ca1b8a2d6658b84f9af2305dd30fcae2b33a10
klindic <klindic94kristijan@gmail.com>
2021-03-23 00:29:03 +0100

    functional Button and Input components

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template functional>
+    <button :type="props.type" class="w-full py-4 bg-green-500 text-center text-white rounded-md">
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        type: String
+    }
+}
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<template functional>
+    <div class="relative h-12 input-component mb-5">
+        <input
+            :id="props.id"
+            :type="props.type"
+            :name="props.id"
+            class="h-full w-full border-gray-300 px-2 transition-all border-blue rounded-md"/>
+        <label for="name" class="absolute left-2 transition-all bg-white px-1">
+            {{ props.label }}
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: String,
+        label: String,
+        type: String
+    }
+}
+</script>
